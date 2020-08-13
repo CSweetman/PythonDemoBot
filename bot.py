8295d7e721c6c7d7488c2c5904d42fcc99ef6c8a
Christopher Sweetman <chrisjdsweetman@gmail.com>
2020-08-13 00:58:50 -0500

    Changes: Added command functionalties such as ping and 8ball that the bot responds to when these commands are accompanied by the prefix. Security concerns: Remember to delete the code before pushing to github '

--- bot.py
+++ bot.py
@@ -1,12 +1,8 @@
 import discord
+import random
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
-
-@client.event
-async def on_ready():
-    print("Bot is ready.")
-
 
 """
 #events are piece of code that runs when a specific action that has happened
@@ -19,5 +15,39 @@ async def on_member_remove(member):
     print(f'{member} has left a server')
 """
 
-client.run("")
 
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+#context is passed in automatically
+#Command piece of code when user tells bot to do something
+#parameters can change attribute of the command 
+@client.command()   
+async def ping(ctx):   
+    await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
+
+@client.command(aliases=['8ball'])  #aliases
+async def _8ball(ctx, *, question):     #asterick will take multiple arguments as 1.
+    responses = ["It is certain.",
+                 "It is decidely so.",
+                 "Without a doubt",
+                 "Yes - definitely.",
+                 "You may rely on it.",
+                 "As I see it, yes.",
+                 "Most likely.",
+                 "Outlook good.",
+                 "Yes.",
+                 "Signs point to yes.",
+                 "Reply haze, try again.",
+                 "Ask again later.",
+                 "Better not tell you now.",
+                 "Cannot predict now.",
+                 "Don't count on it.",
+                 "My reply is no.",
+                 "My sources say no.",
+                 "Outlook not so good.",
+                 "Very doubtful."]
+    await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
+
+client.run("NzQzMjg1ODY4Mjg1OTE5MjYy.XzSc6A.z012CFBK3yDELEeBqFcE-BUfh7k")
